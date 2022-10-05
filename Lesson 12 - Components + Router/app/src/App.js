17d819c5844735e1f6cee049a2a15cd6ab9ad51a
ArnavMeh <mehraarn000@gmail.com>
2022-10-05 16:14:30 -0400

    init

--- Lesson 12 - Components + Router/app/src/App.js
+++ Lesson 12 - Components + Router/app/src/App.js
@@ -1,34 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
 import Home from './components/Home'
 import Resume from './components/Resume';
-import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom'
 import User from './components/User';
-
-
-const About = (props) => {
-  console.log({props})
-  return (
-    <div>
-      <div
-        style={{
-          margin: '10px 0',
-          display: 'flex',
-          justifyContent: 'space-between'
-        }}
-      >
-        About Page
-        <Link to='/about/contact'>
-          Contact
-        </Link>
-        <Link to='/about/founding'>
-          Founding
-        </Link>
-      </div>
-      <Outlet/>
-    </div>
-  )
-}
+import About from './components/About';
 const Contact = () => <div>Contact</div>
 const Founding = () => <div>Founding</div>
 
@@ -43,7 +19,7 @@ function App() {
         <Link to='/resume'>
           Resume
         </Link>
-        <Link to='/home'>
+        <Link to='/'>
           Home
         </Link>
         <Link to='/about'>
@@ -60,7 +36,7 @@ function App() {
       <Routes>
         {/* BASIC ROUTES */}
         <Route
-          path="home"
+          index // equivalent to path=""
           element={<Home/>}
         />
         <Route
@@ -70,16 +46,16 @@ function App() {
         
         {/* NESTED ROUTING */}
         <Route
-          path="about"
-          element={<About/>} // route: /about
+          path="about" // route: /about
+          element={<About/>} // NOTE: see that it contains <Outlet/>, and that contact and founding will be rendered there
         >
           <Route
-            path="contact"
-            element={<Contact/>} // route: /about/contact
+            path="contact" // route: /about/contact
+            element={<Contact/>} 
           />
           <Route
-            path="founding"
-            element={<Founding/>} // route: /about/founding
+            path="founding" // route: /about/founding
+            element={<Founding/>} 
           />
         </Route>
         
